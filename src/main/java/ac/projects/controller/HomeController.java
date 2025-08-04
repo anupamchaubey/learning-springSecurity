0398e5dda43084c516f2383b350e4bab40d3f389
lostannu <icyanupam@gmail.com>
2025-08-04 21:23:41 +0530

    added user registration ... changes in SecurityConfig file added a new UserController... and some minor changes. now users can register and check the login log out functionality.

--- src/main/java/ac/projects/controller/HomeController.java
+++ src/main/java/ac/projects/controller/HomeController.java
@@ -3,6 +3,7 @@ package ac.projects.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
