c2133748737fefb03e6699b85617759bbe0386e8
lostannu <icyanupam@gmail.com>
2025-08-14 19:00:05 +0530

    changing index.html

--- src/main/java/ac/projects/controller/HomeController.java
+++ src/main/java/ac/projects/controller/HomeController.java
@@ -3,19 +3,18 @@ package ac.projects.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "login";
+        return "index";
     }
 
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return "index";
     }
 
     @GetMapping("/user")
